c8c78e6467497f9213933a5201882215b07542d0
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2022-10-26 23:54:12 -0400

    Add tiktok downloader

--- src/ekeko.nim
+++ src/ekeko.nim
@@ -1,7 +1,9 @@
 # This is just an example to get you started. A typical binary package
 # uses this file as the main entry point of the application.
 
-import "youtube"
+from "youtube" import nil
+from "tiktok" import nil
 
 when isMainModule:
-  downloadUrl("https://www.youtube.com/watch?v=QtXby3twMmI")
+  # youtube.downloadUrl("https://www.youtube.com/watch?v=QtXby3twMmI")
+  tiktok.downloadUrl("https://www.tiktok.com/@doracastillo83/video/7156303759520304390")
---
+++ src/tiktok.nim
@@ -0,0 +1,32 @@
+import std/[json, osproc]
+import strformat
+
+proc downloadUrl*(url: string) =  
+  let downloadsFolder = "./downloads/tiktok"
+  let savePath = fmt"{downloads_folder}/%(title)s.%(ext)s"  
+  let process = startProcess("./binaries/yt-dlp", args = [url, "-o", savePath, "--print-json"])    
+  let (lines, exitCode) = process.readLines  
+  let jsonNode = parseJson(lines[0])
+
+  let title =  jsonNode["title"].getStr()
+  let extension = jsonNode["ext"].getStr()
+
+  let resizedSavePath = fmt"{downloads_folder}/{title}-720p.mp4"  
+
+  let ffmpegProcess = startProcess("./binaries/ffmpeg-5.1.1-amd64-static/ffmpeg", args = [
+    "-i",
+    fmt"{downloads_folder}/{title}.{extension}",
+    "-c:v",
+    "libx265",
+    "-vf",
+    "scale=1280:-2",
+    resizedSavePath
+  ])
+
+  echo ffmpegProcess.readLines
+
+  while ffmpegProcess.peekExitCode == -1:
+    echo "Still converting video..."
+
+  process.close()
+  ffmpegProcess.close()
